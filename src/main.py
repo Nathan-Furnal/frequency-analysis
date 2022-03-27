c7b9783642158fe886ea068d3c4f6c9267b37f7e
Nathan Furnal <nathan_furnal@hotmail.com>
2022-03-28 00:37:08 +0200

    fix key and add help for users

--- src/main.py
+++ src/main.py
@@ -9,38 +9,63 @@ parser = argparse.ArgumentParser(
 
 subparser = parser.add_subparsers(help="sub-command help", dest="cmd")
 
-sub_encrypt = subparser.add_parser("encrypt", help="encrypt help")
-sub_encrypt.add_argument("-f", "--file", type=Path, required=True)
-sub_encrypt.add_argument("-k", "--key", type=str, required=True)
-sub_encrypt.add_argument("-o", "--output_file", type=Path, required=True)
+sub_encrypt = subparser.add_parser(
+    "encrypt",
+    help="Encrypts an input file with a given key and saves it to the output file.",
+)
+sub_encrypt.add_argument(
+    "-f", "--file", type=Path, required=True, help="the input file."
+)
+sub_encrypt.add_argument(
+    "-k", "--key", type=str, required=True, help="The key to encode the file."
+)
+sub_encrypt.add_argument(
+    "-o", "--output_file", type=Path, required=True, help="the output file."
+)
 
-sub_decrypt = subparser.add_parser("decrypt", help="decrypt help")
-sub_decrypt.add_argument("-f", "--file", type=Path, required=True)
-sub_decrypt.add_argument("-k", "--key", type=str, required=True)
-sub_decrypt.add_argument("-o", "--output_file", type=Path, required=True)
+sub_decrypt = subparser.add_parser(
+    "decrypt",
+    help="Decrypts an input file with a given key and saves it to the output file.",
+)
+sub_decrypt.add_argument(
+    "-f", "--file", type=Path, required=True, help="the input file."
+)
+sub_decrypt.add_argument(
+    "-k", "--key", type=str, required=True, help="the key to decode the file."
+)
+sub_decrypt.add_argument(
+    "-o", "--output_file", type=Path, required=True, help="the output file."
+)
 
-sub_freq = subparser.add_parser("freq", help="freq help")
-sub_freq.add_argument("-f", "--file", type=Path, required=True)
-sub_freq.add_argument("-g", "--guess", type=int, required=True)
+sub_freq = subparser.add_parser(
+    "freq",
+    help="Uses frequency analysis to decrypt a file encoded with a Caesar or Vigenère cipher. A guess is required for the length of the keyword.",
+)
+sub_freq.add_argument("-f", "--file", type=Path, required=True, help="the input file.")
+sub_freq.add_argument(
+    "-g",
+    "--guess",
+    type=int,
+    required=True,
+    help="Range of values to try for the length of the keyword. A value of 5 will attempt to find the length from 1 to 4.",
+)
 
 
 def main():
     args = parser.parse_args()
     if args.cmd == "encrypt":
-        enc_args = sub_encrypt.parse_args()
-        encryptor = Cipherer(Path(enc_args.f), False)
-        encryptor.encrypt(enc_args.k)
-        encryptor.to_file(Path(enc_args.o), True)
+        encryptor = Cipherer(Path(args.file), False)
+        encryptor.encrypt(args.key)
+        encryptor.to_file(Path(args.output_file), True)
     elif args.cmd == "decrypt":
-        dec_args = sub_decrypt.parse_args()
-        decryptor = Cipherer(Path(dec_args.f), True)
-        decryptor.decrypt(dec_args.k)
-        decryptor.to_file(Path(dec_args.o), False)
+        decryptor = Cipherer(Path(args.file), True)
+        decryptor.decrypt(args.key)
+        decryptor.to_file(Path(args.output_file), False)
     elif args.cmd == "freq":
-        freq_args = sub_freq.parse_args()
-        freq_analyzer = FrequencyAnalyzer(Path(freq_args.f))
-        freq_analyzer.guess_length_kw(freq_args.g)
-        freq_analyzer()
+        freq_analyzer = FrequencyAnalyzer(Path(args.file))
+        freq_analyzer.guess_length_kw(args.guess)
+        freq_analyzer.guess_kw()
+        print(f"The guessed key is: {freq_analyzer.keyword}")
     else:
         print("Use one of [encrypt|decrypt|freq]")
 
